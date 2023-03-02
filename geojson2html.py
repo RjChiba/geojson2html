55407cc845aabedc8bfd6e8b0df0c817da3f41a0
RjChiba <ryuji.chiba.5185@gmail.com>
2023-03-02 20:44:09 +0900

    update: ver1.0

--- geojson2html.py
+++ geojson2html.py
@@ -9,91 +9,86 @@ import sys, os
 #
 #==============================================
 
-class Polygon:
+class Path:
 	def __init__(self, position, viewbox):
-		self.polygon = position
+		self.path = position
 		self.box = viewbox
 
+def updateViewbox(box, p:Path):
+	posH = max(box[0], p.box[0])
+	posB = min(box[1], p.box[1])
+	posL = min(box[2], p.box[2])
+	posR = max(box[3], p.box[3])
+	box = [posH,posB,posL,posR]
 
-def geojson2html(geo, key=None):
+	return box
+
+
+def polygon2Path(dtype, mpoly):
+	posH, posB, posL, posR = 0,1e10,1e10,0
+	pathstr = ""
+
+	if dtype == "Polygon":
+		mpoly = [mpoly]
+	
+	for i in range(len(mpoly)):
+		for poly in mpoly[i]:
+			pathstr += f"M {poly[0][0]},{poly[0][1]}"
+
+			for pos in poly[1:]:
+				pathstr += f" L {pos[0]},{pos[1]}"
+				posH = max(posH, pos[1])
+				posB = min(posB, pos[1])
+				posL = min(posL, pos[0])
+				posR = max(posR, pos[0])
+
+	viewbox = [posH,posB,posL,posR]
+	
+	path = Path(pathstr, viewbox)
+
+	return path
+
+
+def geojson2html_v2(geo, key=None):
 	svghtml = "<svg viewbox=\"#viewbox\" version=\"1.1\" xmlns=\"http://www.w3.org/2000/svg\" style=\"transform: scale(1,-1);\">\n#polygon</svg>"
 	polygonhtml = ""
 	vb = [0,1e10,1e10,0]
 
 	for feature in geo.get("features",[]):
-
-		# get name 
+		
+		# get objects
 		props = feature.get("properties", {})
 		name = props.get("name", "")
 		fullname = props.get("fullname",name)
 
-		if key == None or name in key:
-			ps = geojson2strpoly(feature)
+		if key != None and name not in key:
+			continue
 
-			for (i,p) in enumerate(ps):
-				# add html polygon
-				polygonhtml += f"<polygon id=\"{name}_{i+1}\" points=\"{p.polygon}\"><title>{fullname}</title></polygon>\n"
+		geo = feature.get("geometry",{})
+		geocors = geo.get("coordinates",[])
 
-				# update svg viewbox
-				posH = max(vb[0], p.box[0])
-				posB = min(vb[1], p.box[1])
-				posL = min(vb[2], p.box[2])
-				posR = max(vb[3], p.box[3])
-				vb = [posH,posB,posL,posR]
+		dtype = geo.get("type", None)
+
+		if dtype != "Polygon" and dtype != "MultiPolygon":
+			print(f"Ops! {dtype} can not be handled!")
+			continue
+
+		p = polygon2Path(dtype, geocors)
+
+		vb = updateViewbox(vb, p)
+
+		polygonhtml += f"<path id=\"{fullname}\" d=\"{p.path}\"><title>{fullname}</title></path>\n"
 
 	svghtml = svghtml.replace("#viewbox", f"{vb[2]} {vb[1]} {vb[3]-vb[2]} {vb[0]-vb[1]}")
 	svghtml = svghtml.replace("#polygon", polygonhtml)
 
 	return svghtml
-			
-
-def geojson2strpoly(feature):
-	# list of the polygon
-	polygons = set([])
-
-	# get coordinates
-	geo = feature.get("geometry",{})
-	dtype = geo.get("type", None)
-	geocors = geo.get("coordinates",[])
-
-	if dtype == "Polygon":
-		polygons |= polygon2polygon(geocors)
-
-	elif dtype == "MultiPolygon":
-		for cor in geocors:
-			polygons |= polygon2polygon(cor)
-
-	else:
-		print(
-				f"Ops! {dtype} can not be handled!"
-			)
-
-	return polygons
-
-def polygon2polygon(poly):
-	polygons = set([])
-
-	for cor in poly:
-		polystr = ""
-		posH, posB, posL, posR = 0,1e10,1e10,0
-
-		for position in cor:
-			polystr += f"{position[0]},{position[1]} "
-			posH = max(posH, position[1])
-			posB = min(posB, position[1])
-			posL = min(posL, position[0])
-			posR = max(posR, position[0])
-
-		viewbox = [posH,posB,posL,posR]
-
-		polygons.add(Polygon(polystr, viewbox))
-
-	return polygons
-
 
 if __name__ == "__main__":
 	import requests
 	import json
+
+	html = "<!DOCTYPE html><html lang=\"en\"><head><meta charset=\"UTF-8\"><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\"><title>Document</title></head><style>svg {height: 80vh;margin: 0 10vw;border: 1px solid black;}path,polygon {fill: white;stroke: black;stroke-width: 0.01;}path:hover,polygon:hover {fill: black;transition: 0.5s;}</style><body>#svg</body></html>"
 
 	def getGeoJsonChina():
 		url = "https://geojson.cn/api/data/china.json"
@@ -105,8 +100,8 @@ if __name__ == "__main__":
 		return geo
 
 	geo = getGeoJsonChina()
-	svghtml = geojson2html(geo)
+	html = html.replace("#svg", geojson2html_v2(geo))
 	fp = "./geo.html"
 	
 	with open(fp, 'w') as f:
-		f.write(svghtml)
+		f.write(html)
